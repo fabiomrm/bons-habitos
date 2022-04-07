2d04e4a2713dd840efeb26e43f627380dfd37f08
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-07 16:12:45 -0300

    pós-reunião 07/04 - 14h

--- src/com/bonshabitos/services/ValidatorService.java
+++ src/com/bonshabitos/services/ValidatorService.java
@@ -4,15 +4,13 @@ import com.bonshabitos.interfaces.Validator;
 
 public class ValidatorService implements Validator {
 
+	public ValidatorService() {
+
+	}
+
 	@Override
-	public boolean validate(String identifer) {
-		//Verifica se o número tem 11 dígitos
-		
-		//Validação do primeiro dígito
-		//1 - Multiplica os 9 primeiros dígitos pela sequência decrescente de 10 até 2
-		//2 - Soma o resultado, multiplca por 10, divide por 11 e armazena o resto
-		
-		//3 - SE PRIMEIRO
+	public boolean validate(String identifier) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
