964493cbbd93581fc85cb5765aa0743d99a38ccb
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-09 05:59:20 -0300

    Validação CPF

--- src/com/bonshabitos/services/Test.java
+++ src/com/bonshabitos/services/Test.java
@@ -7,11 +7,13 @@ public class Test {
 	// 5 * 10 + 2 * 9 + 9 * 8 + 9 * 7 + 8 * 6 + 2 * 5 + 2 * 4 + 4 * 3 + 7 * 2
 
 	public static void main(String[] args) {
-		String cpf = "52998224725";
+		String cpf = "70164328076";
 		int resto;
 		if (cpf.length() != 11) {
 			System.out.println("\nNúmero de cpf inválido: ");
-		} else {
+		}
+
+		else {
 			String vetor[] = cpf.split("");
 			int contador = 11;
 			int soma = 0;
@@ -25,9 +27,28 @@ public class Test {
 			if (resto == 10) {
 				resto = 0;
 			}
-			if(resto == Integer.parseInt(vetor[9])) {
+			if (resto == Integer.parseInt(vetor[9])) {
 				System.out.println("Valor do primeiro dígito validado!");
 			}
+
+			// Verificando o segundo dígito
+			contador = 12;
+			soma = 0;
+			for (int i = 0; i < 10; i++) {
+				System.out.println(vetor[i] + " * " + (contador - 1));
+				soma += Integer.parseInt(vetor[i]) * (contador - 1);
+				contador--;
+			}
+			System.out.println(soma);
+			resto = soma * 10 % 11;
+			if(resto == 10) {
+				resto =0;
+			}
+			
+			if(resto == Integer.parseInt(vetor[10])) {
+				System.out.println("Valor do segundo dígito validado");
+			}
+
 		}
 
 	}
